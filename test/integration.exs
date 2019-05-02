450fc69d8b3e826b9f9ab646c965907eb89d206f
Stephen Bussey <sb8244@cs.ship.edu>
2019-05-01 22:17:53 -0400

    A test that can be run with `mix run test/integration.exs`

---
+++ test/integration.exs
@@ -0,0 +1,24 @@
+IO.puts "start"
+metrics = [
+  [:test],
+  [:other, :test],
+  [:other, :test2]
+]
+
+# pool_size defaults to num schedulers
+# prefix defaults to TelemetryAsync.Shard
+
+Supervisor.start_link([{TelemetryAsync.ShardSupervisor, pool_size: 4, prefix: :steve}], [strategy: :one_for_one])
+Supervisor.start_link([{TelemetryAsync.Handler, pool_size: 4, prefix: :steve, metrics: metrics}], [strategy: :one_for_one])
+
+Supervisor.start_link([TelemetryAsync.ShardSupervisor], [strategy: :one_for_one])
+Supervisor.start_link([{TelemetryAsync.Handler, metrics: metrics}], [strategy: :one_for_one])
+
+:telemetry.attach(:test, [:async, :test], fn a, b, c, d ->
+  IO.inspect {a,b,c,d}
+end, nil)
+:telemetry.execute([:test], %{}, %{})
+
+Process.sleep(100)
+
+IO.puts "done"
